bd04efbd6f7d560da7e91d47f193797934de5f45
Jacob <jacob.jonkman@gmail.com>
2019-11-05 14:32:36 +0100

    Now reading music condition from firebase firestore

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { AssignmentService } from '../../services/assignment.service';
 import { Assignment } from './test-step/test-step.component';
 import { Observable } from 'rxjs';
+import { map, first, shareReplay } from 'rxjs/operators';
 import { CodeService } from '../../services/code.service';
 import { ParticipantService } from '../../services/participant.service';
 import { Participant } from '../../models/participant';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-test',
@@ -13,7 +15,7 @@ import { Participant } from '../../models/participant';
 })
 export class TestComponent {
   assignmentNumber = 0;
-  listeningToMusic = false;
+  listeningToMusic$: Observable<boolean>;
   assignments$: Observable<Assignment[]>;
   code: string;
   started = false;
@@ -24,16 +26,23 @@ export class TestComponent {
 
   constructor(private assignmentService: AssignmentService,
               private codeService: CodeService,
-              private participantService: ParticipantService) {
+              private participantService: ParticipantService,
+              private afs: AngularFirestore) {
     this.assignments$ = this.assignmentService.assignments$;
     this.code = this.codeService.getCode(this.assignmentNumber);
     this.numCodes = this.codeService.numberOfCodes;
     this.participant$ = this.participantService.participant$;
+    
+    this.listeningToMusic$ = this.afs.collection(`params`).valueChanges().pipe(
+      first(),
+      map(params => params[0]['ßmusic']),
+      shareReplay(1)
+    );
   }
 
-  beginTest(): void {
+  beginTest(listeningToMusic: boolean): void {
     this.started = true;
-    this.participantId$ = this.participantService.newParticipant(this.listeningToMusic);
+    this.participantId$ = this.participantService.newParticipant(listeningToMusic);
   }
 
   changeAssignment(nextAssignment: number): any {
